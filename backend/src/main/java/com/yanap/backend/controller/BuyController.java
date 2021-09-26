f608de328c77fddc93724a3c9d994d76ef142493
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 06:13:23 +0900

    買って即返金するようにしてみた

--- backend/src/main/java/com/yanap/backend/controller/BuyController.java
+++ backend/src/main/java/com/yanap/backend/controller/BuyController.java
@@ -3,6 +3,7 @@ package com.yanap.backend.controller;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
+import com.stripe.model.Refund;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +37,14 @@ public class BuyController {
         try {
             Charge charge = Charge.create(chargeMap);
             System.out.println(charge);
+            
+            System.out.println("======================");
+            
+            Map<String, Object> refundMap = new HashMap<String, Object>();
+            refundMap.put("charge", charge.getId());    // IDを投げると返金できるので、システム上から返金したい場合は控えておく必要がある
+            Refund refund = Refund.create(refundMap);
+            System.out.println(refund);
+            
         } catch (StripeException e) {
             e.printStackTrace();
             return new Response(false);
