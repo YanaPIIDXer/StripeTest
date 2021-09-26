11df5b3605c2c9be4c10fccff41a68f0c1c61d35
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 05:16:22 +0900

    違うものを入れていたので正しいものを入れ直した

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
+    <h1>決済テスト</h1>
+    <hr />
+    <div id="card-elem"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data: function () {
+    return {
+    };
+  },
+  mounted: function () {
+  }
 }
 </script>
 
